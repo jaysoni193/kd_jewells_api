a6590c47b546798aef3bea443c5bb57284a8b9d9
Jay Soni <js2001193@gmail.com>
2025-08-29 23:25:50 +0530

    this is my First Commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors());
+
+mongoose.connect("mongodb+srv://Jaysoni:Jaysoni001@cluster0.5ozekes.mongodb.net/KD_Jewells?retryWrites=true&w=majority&appName=Cluster0");
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
